3797396910821159b0222528e44ef61fc7629538
JoseDeJesus002 <89945300+JoseDeJesus002@users.noreply.github.com>
2022-02-25 08:52:36 -0600

    Update

--- Ejercicio_3_0.cpp
+++ Ejercicio_3_0.cpp
@@ -12,119 +12,119 @@ void gotoxy(int x,int y){
 	dwPos.Y= y;  
 	SetConsoleCursorPosition(hcon,dwPos);  
 }
+
+struct nodo
+{
+	int nro;
+	struct nodo *sgte;
+};
+
+struct cola
+{
+	nodo *delante;
+	nodo *atras  ;
+};
+
+void encolar( struct cola &q, int valor )
+{
+	struct nodo *aux = new(struct nodo);
 	
-	struct nodo
-	{
-		int nro;
-		struct nodo *sgte;
-	};
+	aux->nro = valor;
+	aux->sgte = NULL;
 	
-	struct cola
-	{
-		nodo *delante;
-		nodo *atras  ;
-	};
+	if( q.delante == NULL)
+		q.delante = aux;   
+	else
+		(q.atras)->sgte = aux;
 	
-	void encolar( struct cola &q, int valor )
-	{
-		struct nodo *aux = new(struct nodo);
-		
-		aux->nro = valor;
-		aux->sgte = NULL;
-		
-		if( q.delante == NULL)
-			q.delante = aux;   
-		else
-			(q.atras)->sgte = aux;
-		
-		q.atras = aux;       
-	}    
+	q.atras = aux;       
+}    
+
+int desencolar( struct cola &q )
 	
-	int desencolar( struct cola &q )
-		
-	{
-		int num ;
-		struct nodo *aux ;
-		
-		aux = q.delante;      
-		num = aux->nro;
-		q.delante = (q.delante)->sgte;
-		delete(aux);          
-		
-		return num;
-	} 
+{
+	int num ;
+	struct nodo *aux ;
 	
-	void menu()
+	aux = q.delante;      
+	num = aux->nro;
+	q.delante = (q.delante)->sgte;
+	delete(aux);          
+	
+	return num;
+} 
+
+void menu()
+{
+	struct cola q;
+	
+	q.delante = NULL;
+	q.atras   = NULL;
+	
+	int dato; 
+	int op;    
+	int x ;    
+	int a =0;
+	
+	do
 	{
-		struct cola q;
-		
-		q.delante = NULL;
-		q.atras   = NULL;
-		
-		int dato; 
-		int op;    
-		int x ;    
-		int a =0;
-		
-		do
-		{
-			b:
-				system("cls");
-			gotoxy(40,4);cout<<  " _____________________________________________________________________";
-			gotoxy(40,5);cout<< "| REGISTRARSE PARA FICHA                                          | 1 |";              
-			gotoxy(40,6);cout<< "|_________________________________________________________________|___|";
-			gotoxy(40,7);cout<< "| ATENDER                                                         | 2 |";
-			gotoxy(40,8);cout<< "|_________________________________________________________________|___|";
-			gotoxy(40,9);cout<< "| SALIR                                                           | 3 |";
-			gotoxy(40,10);cout<<"|_________________________________________________________________|___|";
-			gotoxy(40,11);cout<<"| INGRESA LA OPCION A REALIZAR                                    |   |";
-			gotoxy(40,12);cout<<"|_________________________________________________________________|___|";gotoxy(108,11);cin>>op;
-			
-			switch(op)
-			{
-			case 1:
-				if(op=1){
-					a=a+1;
-					dato=a;
-				}
-				cout<< "\n Tu numero de ficha es: "<<dato<<endl;
-				cout<< "\n";
-				encolar( q, dato );
-				cout<<"Espera a que te llamen";
-				break;
-				
-			case 2:
-				x = desencolar( q );
-				cout<<"\n\n\t\tEl numero de ficha "<< x <<" a sido atendido\n\n";
-				break;
-				
-			case 3:
-				cout<<"Gracias por tu preferencia";
-				break;
-			}
-			
-			cout<<endl<<endl;
-			system("pause");  
+		b:
 			system("cls");
-			
-		}while(op!=3);
-	}
-	
-	int main(){
-		for(int a=40;a<=75;a++){
-			system("color A");
-			
-			int d=115;
-			d=d-a;
-			gotoxy(40,6);cout<<"               BIENVEDO              ";
-			gotoxy(40,7);cout<<"/////////////////////////////////////";
-			gotoxy(a,8);cout<<"/";
-			gotoxy(d,9);cout<<"/";
-			gotoxy(40,10);cout<<"////////////////////////////////////";
-			
-			Sleep(5);
-		}
-		menu();
-		return 0;
-	}
+		gotoxy(5,4);cout<<  " _____________________________________________________________________";
+		gotoxy(5,5);cout<< "| REGISTRARSE PARA FICHA                                          | 1 |";              
+		gotoxy(5,6);cout<< "|_________________________________________________________________|___|";
+		gotoxy(5,7);cout<< "| ATENDER                                                         | 2 |";
+		gotoxy(5,8);cout<< "|_________________________________________________________________|___|";
+		gotoxy(5,9);cout<< "| SALIR                                                           | 3 |";
+		gotoxy(5,10);cout<<"|_________________________________________________________________|___|";
+		gotoxy(5,11);cout<<"| INGRESA LA OPCION A REALIZAR                                    |   |";
+		gotoxy(5,12);cout<<"|_________________________________________________________________|___|";gotoxy(73,11);cin>>op;
 		
+		switch(op)
+		{
+		case 1:
+			if(op=1){
+				a=a+1;
+				dato=a;
+			}
+			cout<< "\n";
+			cout<< "\n Tu numero de ficha es: "<<dato<<endl;
+			cout<< "\n";
+			encolar( q, dato );
+			cout<<"Espera a que te llamen";
+			break;
+			
+		case 2:
+			cout<< "\n";
+			x = desencolar( q );
+			cout<<"\n\n\t\tEl numero de ficha "<< x <<" a sido atendido\n\n";
+			break;
+			
+		case 3:
+			cout<< "\n";
+			cout<<"Gracias por tu preferencia";
+			break;
+		}
+		
+		cout<<endl<<endl;
+		system("pause");  
+		system("cls");
+		
+	}while(op!=3);
+}
+
+int main(){
+	for(int a=5;a<=40;a++){
+		system("color B");
+		
+		gotoxy(5,6);cout<<"               BIENVEDO              ";
+		gotoxy(5,7);cout<<"/////////////////////////////////////";
+		gotoxy(a,8);cout<<"/";
+		gotoxy(5,9);cout<<"////////////////////////////////////";
+		
+		Sleep(10);
+	}
+	menu();
+	return 0;
+}
+
